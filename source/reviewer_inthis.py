c884e705ede52f9d45fa0edafa744b6009ffa421
Theodor Lindberg <theodor.lindberg@liu.se>
2026-03-23 13:20:20 +0100

    Don't check in-this-work in conclusions

--- source/reviewer_inthis.py
+++ source/reviewer_inthis.py
@@ -6,7 +6,6 @@ from printer import Printer
 
 class Reviewer_Inthis(Reviewer):
     _PATTERN_ABSTRACT_START = re.compile(r"\\begin\{abstract\}")
-    _PATTERN_CONCLUSIONS_START = re.compile(r"\\section\{Conclusion", re.IGNORECASE)
     _PATTERN_THIS_WORK = re.compile(
         r"\bthis\s+(?:\w+\s+)*(work|brief|paper|manuscript)s?\b",
         re.IGNORECASE,
@@ -16,10 +15,7 @@ class Reviewer_Inthis(Reviewer):
         self.printer = printer
         self.abstract_context_start = False
         self.check_abstract_first_line = False
-        self.conclusions_context_start = False
-        self.check_conclusions_first_line = False
         self.abstract_check = Status.UNCHECKED
-        self.conclusions_check = Status.UNCHECKED
 
     def process_line(self, line_no: int, line: str) -> None:
         if self.abstract_check == Status.UNCHECKED:
@@ -39,34 +35,12 @@ class Reviewer_Inthis(Reviewer):
                 else:
                     self.abstract_check = Status.PASSED
 
-        if self.conclusions_check == Status.UNCHECKED:
-            if self._PATTERN_CONCLUSIONS_START.search(line):
-                self.check_conclusions_first_line = True
-                return
-
-        if self.check_conclusions_first_line:
-            if line and line[0] != '%': # Skip empty lines and comments
-                self.check_conclusions_first_line = False
-                if self._PATTERN_THIS_WORK.search(line):
-                    self.conclusions_check = Status.FAILED
-                    self.printer.print_no(
-                        line_no,
-                        "First line in conclusions should not contain 'in this work/brief/paper/manuscript' or 'this paper'.",
-                    )
-                else:
-                    self.conclusions_check = Status.PASSED
-
 
     def get_summary(self) -> str:
         return ""
 
     def get_status(self) -> Status:
-        if self.abstract_check == Status.PASSED and self.conclusions_check == Status.PASSED:
-            return Status.PASSED
-        elif self.abstract_check == Status.FAILED or self.conclusions_check == Status.FAILED:
-            return Status.FAILED
-        else:
-            return Status.UNCHECKED
+        return self.abstract_check
         
     def get_name(self) -> str:
         return "InThisWork"
